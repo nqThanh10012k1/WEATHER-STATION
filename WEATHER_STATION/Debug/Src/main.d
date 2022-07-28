3ca429290c295641b42c9c5122b6a4c93d01a563
ThanhNguyen <thanhnq.10012k1@gmail.com>
2022-07-28 19:45:05 +0700

    Completed

---
+++ WEATHER_STATION/Debug/Src/main.d
@@ -0,0 +1,33 @@
+Src/main.o: ../Src/main.c \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_spi.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_i2c.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_tim.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_adc.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h \
+ D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_spi.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_i2c.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_tim.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_adc.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
+D:/STM32/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
